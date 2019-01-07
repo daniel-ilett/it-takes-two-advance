30bc40dc5d661745c4fa3738fb883a6b8f5b4b24
Daniel Ilett <danielilett@gmail.com>
2019-01-07 01:06:21 +0000

    Added struct to hold sprite position data.

---
+++ src/data/sprite_obj.h
@@ -0,0 +1,12 @@
+// Struct that holds data about sprites.
+
+#ifndef SPRITE_OBJ_H
+#define SPRITE_OBJ_H
+
+typedef struct SpriteObj
+{
+	u8 posX;
+	u8 posY;
+} SpriteObj;
+
+#endif
--- src/itt-adv.c
+++ src/itt-adv.c
@@ -47,8 +47,29 @@ int main()
 
 	// Copy the ball pixel data to the first location in sprite data.
 	memcpy((u16*)0x06014000, &tx_EarthData, sizeof(tx_EarthData));
-
+	memcpy((u16*)0x06014100, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06014200, &tx_WaterData, sizeof(tx_WaterData));
+	memcpy((u16*)0x06014300, &tx_OreData, sizeof(tx_OreData));
 	memcpy((u16*)0x06014400, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06014500, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06014600, &tx_VampireData, sizeof(tx_VampireData));
+	memcpy((u16*)0x06014700, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06014800, &tx_FireData, sizeof(tx_FireData));
+
+	memcpy((u16*)0x06014900, &tx_BirdData, sizeof(tx_BirdData));
+	memcpy((u16*)0x06014a00, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06014b00, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06014c00, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06014d00, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06014e00, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06014f00, &tx_ZombieData, sizeof(tx_ZombieData));
+	memcpy((u16*)0x06015000, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06015100, &tx_AlcoholData, sizeof(tx_AlcoholData));
+
+	memcpy((u16*)0x06015200, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06015300, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06015400, &tx_FireData, sizeof(tx_FireData));
+	memcpy((u16*)0x06015500, &tx_FireData, sizeof(tx_FireData));
 
 	/*
 	memcpy((u16*)0x06014100, &barData,  	sizeof(barData));
@@ -65,15 +86,96 @@ int main()
 	memcpy((u16*)0x06014d00, &number9Data, 	sizeof(number9Data));
 	*/
 
-	// Create a test sprite for the Earth sprite.
-	sprites[0].attribute0 = COLOR_256 | SQUARE | 80;
-	sprites[0].attribute1 = SIZE_16 | 80;
+	// First row of item tray sprites.
+	sprites[0].attribute0 = COLOR_256 | SQUARE | 10 * 8;
+	sprites[0].attribute1 = SIZE_16 | 2 * 8;
 	sprites[0].attribute2 = 512;
 
-	// Create a test sprite for the Fire sprite.
-	sprites[1].attribute0 = COLOR_256 | SQUARE | 80;
-	sprites[1].attribute1 = SIZE_16 | 100;
-	sprites[1].attribute2 = 512 + 32;
+	sprites[1].attribute0 = COLOR_256 | SQUARE | 10 * 8;
+	sprites[1].attribute1 = SIZE_16 | 5 * 8;
+	sprites[1].attribute2 = 512 + 8;
+
+	sprites[2].attribute0 = COLOR_256 | SQUARE | 10 * 8;
+	sprites[2].attribute1 = SIZE_16 | 8 * 8;
+	sprites[2].attribute2 = 512 + 8 * 2;
+
+	sprites[3].attribute0 = COLOR_256 | SQUARE | 10 * 8;
+	sprites[3].attribute1 = SIZE_16 | 11 * 8;
+	sprites[3].attribute2 = 512 + 8 * 3;
+
+	sprites[4].attribute0 = COLOR_256 | SQUARE | 10 * 8;
+	sprites[4].attribute1 = SIZE_16 | 14 * 8;
+	sprites[4].attribute2 = 512 + 8 * 4;
+
+	sprites[5].attribute0 = COLOR_256 | SQUARE | 10 * 8;
+	sprites[5].attribute1 = SIZE_16 | 17 * 8;
+	sprites[5].attribute2 = 512 + 8 * 5;
+
+	sprites[6].attribute0 = COLOR_256 | SQUARE | 10 * 8;
+	sprites[6].attribute1 = SIZE_16 | 20 * 8;
+	sprites[6].attribute2 = 512 + 8 * 6;
+
+	sprites[7].attribute0 = COLOR_256 | SQUARE | 10 * 8;
+	sprites[7].attribute1 = SIZE_16 | 23 * 8;
+	sprites[7].attribute2 = 512 + 8 * 7;
+
+	sprites[8].attribute0 = COLOR_256 | SQUARE | 10 * 8;
+	sprites[8].attribute1 = SIZE_16 | 26 * 8;
+	sprites[8].attribute2 = 512 + 8 * 8;
+
+	// Second row of item tray sprites.
+	sprites[9].attribute0 = COLOR_256 | SQUARE | 13 * 8;
+	sprites[9].attribute1 = SIZE_16 | 2 * 8;
+	sprites[9].attribute2 = 512 + 8 * 9;
+
+	sprites[10].attribute0 = COLOR_256 | SQUARE | 13 * 8;
+	sprites[10].attribute1 = SIZE_16 | 5 * 8;
+	sprites[10].attribute2 = 512 + 8 * 10;
+
+	sprites[11].attribute0 = COLOR_256 | SQUARE | 13 * 8;
+	sprites[11].attribute1 = SIZE_16 | 8 * 8;
+	sprites[11].attribute2 = 512 + 8 * 11;
+
+	sprites[12].attribute0 = COLOR_256 | SQUARE | 13 * 8;
+	sprites[12].attribute1 = SIZE_16 | 11 * 8;
+	sprites[12].attribute2 = 512 + 8 * 12;
+
+	sprites[13].attribute0 = COLOR_256 | SQUARE | 13 * 8;
+	sprites[13].attribute1 = SIZE_16 | 14 * 8;
+	sprites[13].attribute2 = 512 + 8 * 13;
+
+	sprites[14].attribute0 = COLOR_256 | SQUARE | 13 * 8;
+	sprites[14].attribute1 = SIZE_16 | 17 * 8;
+	sprites[14].attribute2 = 512 + 8 * 14;
+
+	sprites[15].attribute0 = COLOR_256 | SQUARE | 13 * 8;
+	sprites[15].attribute1 = SIZE_16 | 20 * 8;
+	sprites[15].attribute2 = 512 + 8 * 15;
+
+	sprites[16].attribute0 = COLOR_256 | SQUARE | 13 * 8;
+	sprites[16].attribute1 = SIZE_16 | 23 * 8;
+	sprites[16].attribute2 = 512 + 8 * 16;
+
+	sprites[17].attribute0 = COLOR_256 | SQUARE | 13 * 8;
+	sprites[17].attribute1 = SIZE_16 | 26 * 8;
+	sprites[17].attribute2 = 512 + 8 * 17;
+
+	// Third row of item tray sprites.
+	sprites[18].attribute0 = COLOR_256 | SQUARE | 16 * 8;
+	sprites[18].attribute1 = SIZE_16 | 76;
+	sprites[18].attribute2 = 512 + 8 * 18;
+
+	sprites[19].attribute0 = COLOR_256 | SQUARE | 16 * 8;
+	sprites[19].attribute1 = SIZE_16 | 100;
+	sprites[19].attribute2 = 512 + 8 * 19;
+
+	sprites[20].attribute0 = COLOR_256 | SQUARE | 16 * 8;
+	sprites[20].attribute1 = SIZE_16 | 124;
+	sprites[20].attribute2 = 512 + 8 * 20;
+
+	sprites[21].attribute0 = COLOR_256 | SQUARE | 16 * 8;
+	sprites[21].attribute1 = SIZE_16 | 148;
+	sprites[21].attribute2 = 512 + 8 * 21;
 
 	// Start the game loop.
 	while(1)
